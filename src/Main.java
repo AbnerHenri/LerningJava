a5033e4632f96970aa10a55ee9c9cfe6720a4a04
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-21 17:32:40 -0300

    Calculando Raiz Quadrada

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,29 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Digite um número: ");
-		double numero1 = sc.nextDouble();
+		System.out.print("Digite o valor de a: ");
+		int a = sc.nextInt();
 		sc.nextLine();
 		
-		System.out.print("Digite outro número: ");
-		double numero2 = sc.nextDouble();
+		System.out.print("Digite o valor de b: ");
+		int b = sc.nextInt();
+		sc.nextLine();
 		
-		
-		double result = numero1 + numero2;
-		
-		System.out.println("O resultado da operação é: " + result);
+		System.out.print("Digite o valor de c: ");
+		int c = sc.nextInt();
 		sc.close();
+		
+		double delta = Math.pow(b, 2.0) - 4*a*c;
+		
+		double raiz = Math.sqrt(Math.abs(delta));
+		
+		double x1 = (-b + raiz) / a;
+		double x2 = (-b - raiz) / a;
+		
+		System.out.println();
+		
+		System.out.printf("O valor de x1: %.4f \n", x1);
+		System.out.printf("O valor de x2: %.4f", x2);
 	}
 
 }
