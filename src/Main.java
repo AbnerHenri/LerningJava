5812b61cc6bca1d6e5cedc36e56189ecea91b231
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-21 16:50:58 -0300

    Calculadora - Soma

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,23 @@
-import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		String product1 = "Computer";
-		String product2 = "Office Desk";
 		
-		int age = 30;
-		int code = 5290;
-		char gender = 'F';
+		Scanner sc = new Scanner(System.in);
 		
-		double price1 = 2100.0;
-		double price2 = 650.50;
-		double measure = 53.234567;
+		System.out.print("Digite um número: ");
+		double numero1 = sc.nextDouble();
+		sc.nextLine();
 		
-		System.out.println("Products:");
-		System.out.println(product1 + ", wich price is $" + price1);
-		System.out.println(product2 + ", wich price is $" + price2);
-		System.out.println();
-		System.out.printf("Record: %d years old, code %d and gender %s %n", age, code, gender);
-		System.out.println();
-		System.out.println("Measue with eight decimal places: " + measure);
-		System.out.printf("Rouded (three decimal places): %.3f %n", measure);
-		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f", measure);	
+		System.out.print("Digite outro número: ");
+		double numero2 = sc.nextDouble();
+		
+		
+		double result = numero1 + numero2;
+		
+		System.out.println("O resultado da operação é: " + result);
+		sc.close();
 	}
 
 }
