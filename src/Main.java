1c2ad2214fb9cb3322daf60b41f9f022723f2733
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-24 12:21:01 -0300

    Calculando área do Triangulo - Com classe

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
+import entities.Triangle;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.println("Insira as medidas do triangulo X:");
 		
-		double ax = sc.nextDouble();
-		double bx = sc.nextDouble();
-		double cx = sc.nextDouble();
+		Triangle x, y;
 		
-		double px = (ax + bx + cx) / 2;
-		double areaxResult = px*((px - ax) * (px - bx) * (px - cx));
-		double areax = Math.sqrt(areaxResult);
+		x = new Triangle();
+		y = new Triangle();
+		
+		 x.a = sc.nextDouble();
+		 x.b = sc.nextDouble();
+		 x.c = sc.nextDouble();
 		
 		System.out.println("Insira as medidas do traingulo Y:");
 		
-		double ay = sc.nextDouble();
-		double by = sc.nextDouble();
-		double cy = sc.nextDouble();
-		
-		double py = (ay + by + cy) / 2;
-		double areayResult = py*((py - ay) * (py - by) * (py - cy));
-		double areay = Math.sqrt(areayResult);
+		 y.a = sc.nextDouble();
+		 y.a = sc.nextDouble();
+		 y.a = sc.nextDouble();
+		 
+		double areay = y.areaResult();
+		double areax = x.areaResult();
 		
 		System.out.printf("A area do triangulo X: %.4f%n", areax);
 		System.out.printf("A area do triangulo Y: %.4f%n", areay);
