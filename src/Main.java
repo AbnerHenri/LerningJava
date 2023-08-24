5cb7b554614cb7cb244d3b51e8621bc274efe9de
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-24 11:44:54 -0300

    Calculando área do Triangulo - Simples

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,34 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Digite o valor da compra: ");
-		double preco = sc.nextDouble();
-		double novoPreco = (preco < 20) ? preco * 0.1 : preco *0.05;
+		System.out.println("Insira as medidas do triangulo X:");
 		
-		System.out.println("Valor do Desconto: " + novoPreco);
-		System.out.println("Valor total: " + (preco - novoPreco));
+		double ax = sc.nextDouble();
+		double bx = sc.nextDouble();
+		double cx = sc.nextDouble();
+		
+		double px = (ax + bx + cx) / 2;
+		double areaxResult = px*((px - ax) * (px - bx) * (px - cx));
+		double areax = Math.sqrt(areaxResult);
+		
+		System.out.println("Insira as medidas do traingulo Y:");
+		
+		double ay = sc.nextDouble();
+		double by = sc.nextDouble();
+		double cy = sc.nextDouble();
+		
+		double py = (ay + by + cy) / 2;
+		double areayResult = py*((py - ay) * (py - by) * (py - cy));
+		double areay = Math.sqrt(areayResult);
+		
+		System.out.printf("A area do triangulo X: %.4f%n", areax);
+		System.out.printf("A area do triangulo Y: %.4f%n", areay);
+		
+		if(areax > areay) {
+			System.out.println("A maior area e a do triangulo X");
+		}else {
+			System.out.println("A maior area e a do traingulo Y");
+		}
 		
 		sc.close();
 	}
