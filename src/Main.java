082edc6a9be25716531767e575460db9d6a521a9
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-28 14:17:48 -0300

    Listando número negativos - Exercício

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,30 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Product;
-
 public class Main {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 			
-		System.out.print("Digite a quantidade de produtos: ");
-		int n = sc.nextInt();
+		System.out.print("Quantos numeros voce vai digitar: ");
+		int quantity = sc.nextInt();
 		
-		Product[] vect = new Product[n];
+		int[] vect = new int[quantity];
 		
-		for(int i = 0; i < n; i++) {
-			System.out.printf("Digite o nome %d do produto: ", (i + 1));
-			String name = sc.next();
-			
-			System.out.printf("Digite o preco do %d produto: ", (i + 1));
-			double price = sc.nextDouble();
-			
-			vect[i] = new Product(name, price);
+		for(int i = 0; i < vect.length ; i++) {
+			System.out.print("Digite um numero: ");
+			vect[i] = sc.nextInt();
 		}
 		
-		double media = 0;
+		System.out.println("Numeros negativos");
 		
-		for(int i = 0; i < n; i++) {
-			 media += vect[i].getPrice();
+		for(int i = 0; i < vect.length; i++) {
+			if(vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
 		}
 		
-		System.out.println("A media da soma dos " + n + " produtos e: " + (media / n));
 		
 		sc.close();
 	}
