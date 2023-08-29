d6e10fb17880604cd8b54fbc52c7e2ecfa8403f2
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-29 09:15:37 -0300

    Cadastro de Funcionários - Exercício

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Emplyoee;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-			
-		System.out.print("Quantos numeros voce vai digitar: ");
+		
+		System.out.print("How many emplyoees will be registered: ");
 		int quantity = sc.nextInt();
+		sc.nextLine();
 		
-		int[] vect = new int[quantity];
+		List<Emplyoee> emplyoee = new ArrayList<>();
 		
-		for(int i = 0; i < vect.length ; i++) {
-			System.out.print("Digite um numero: ");
-			vect[i] = sc.nextInt();
+		for(int i = 0; i < quantity; i++) {
+			System.out.println();
+			System.out.println("Emplyoee #" + (i + 1));
+			System.out.print("Id: ");
+			int id = sc.nextInt();
+			
+			System.out.print("Name: ");
+			String name = sc.next();
+			
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			
+			Emplyoee newEmplyoee = new Emplyoee(id, name, salary);
+			
+			emplyoee.add(newEmplyoee);
 		}
 		
-		System.out.println("Numeros negativos");
-		
-		for(int i = 0; i < vect.length; i++) {
-			if(vect[i] < 0) {
-				System.out.println(vect[i]);
-			}
+		System.out.println();
+		for(Emplyoee element : emplyoee) {
+			System.out.printf("%d, %s, %.2f %n", element.getId(), element.getName(), element.getSalary());
 		}
+		
 		
 		
 		sc.close();
---
+++ src/entities/Emplyoee.java
@@ -0,0 +1,31 @@
+package entities;
+
+public class Emplyoee {
+	private int id;
+	private String name;
+	private double salary;
+	
+	public Emplyoee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
