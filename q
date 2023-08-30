c8134f9f6e2b73c183ced07e9560908b0278b31a
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-08-30 18:25:16 -0300

    Trabalhando com Matrizes - Exercício

---
+++ q
@@ -0,0 +1,13 @@
+[33md6e10fb[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m Cadastro de Funcionários - Exercício
+[33m082edc6[m[33m ([m[1;31morigin/main[m[33m)[m Listando número negativos - Exercício
+[33m27141f2[m Média de Produtos - Usando Arrays com Classes
+[33mcb52df4[m Calculando média de alturas - Usando Arrays
+[33mb86e59e[m Sistema de Depósito e Saque - Exercicio seção 9
+[33mb87b0c6[m Gerenciador de Estoque Simples
+[33m1c2ad22[m Calculando área do Triangulo - Com classe
+[33m5cb7b55[m Calculando área do Triangulo - Simples
+[33mf235e5e[m Calculador de desconto - Operador Ternário
+[33m20b57df[m Comprimentador - Usando Condicionais
+[33ma5033e4[m Calculando Raiz Quadrada
+[33m5812b61[m Calculadora - Soma
+[33m4451037[m Exercicio do curso - Trabalhando com tipos de variaveis
--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,48 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Emplyoee;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("How many emplyoees will be registered: ");
-		int quantity = sc.nextInt();
-		sc.nextLine();
-		
-		List<Emplyoee> emplyoee = new ArrayList<>();
-		
-		for(int i = 0; i < quantity; i++) {
-			System.out.println();
-			System.out.println("Emplyoee #" + (i + 1));
-			System.out.print("Id: ");
-			int id = sc.nextInt();
-			
-			System.out.print("Name: ");
-			String name = sc.next();
-			
-			System.out.print("Salary: ");
-			double salary = sc.nextDouble();
-			
-			Emplyoee newEmplyoee = new Emplyoee(id, name, salary);
-			
-			emplyoee.add(newEmplyoee);
+
+		System.out.print("Digite o número de colunas e linhas: ");
+		int n = sc.nextInt();
+
+		int[][] mat = new int[n][n];
+
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				System.out.print("Digite o número da linha " + (i + 1) + " na " + (j + 1) + "º coluna : ");
+				mat[i][j] = sc.nextInt();
+				if(j == (n - 1)){ System.out.println(); }
+			}
 		}
-		
+
+		for(int i = 0; i < n; i++){
+			System.out.print("Main Diagonal linha " + (i + 1) + " : ");
+			System.out.println(mat[i][i] + " ");
+		}
+
 		System.out.println();
-		for(Emplyoee element : emplyoee) {
-			System.out.printf("%d, %s, %.2f %n", element.getId(), element.getName(), element.getSalary());
+		System.out.println("Números Negativos: ");
+		boolean negatives = false;
+
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				if(mat[i][j] < 0){		
+					int negative = mat[i][j];
+					System.out.println(negative + " ");
+					negatives = true;
+				}
+			}
 		}
-		
-		
-		
+
+		if(!negatives) { System.out.println("Não há números negativos"); }
+
+		System.out.println();
+
 		sc.close();
 	}
 }
